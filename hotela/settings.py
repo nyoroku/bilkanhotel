4e90770383a1fe5b2bd5a6babf345eabfdae9213
nyoroku <nyoroku@example.com>
2026-04-14 06:12:58 +0300

    Fix STATIC_ROOT and add static folder

--- hotela/settings.py
+++ hotela/settings.py
@@ -168,11 +168,11 @@ TIME_FORMAT = 'H:i:s'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
-STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
